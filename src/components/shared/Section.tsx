552010a2a63b7d738c752345395b8d43baa7dc97
Yousef Tarek <youseftarek.business@gmail.com>
2025-06-29 01:07:29 +0300

    update Home component with Section layout

---
+++ src/components/shared/Section.tsx
@@ -0,0 +1,39 @@
+import { cn } from "@/lib/utils";
+import { Link } from "react-router-dom";
+
+type Props = {
+  className?: string;
+  Heading: string;
+  description: string;
+  link?: string;
+  linkHref?: string;
+  children: React.ReactNode;
+};
+
+const Section = ({
+  className,
+  Heading,
+  description,
+  link,
+  linkHref,
+  children,
+}: Props) => {
+  return (
+    <section className={cn("p-3 md:p-6 rounded-lg", className)}>
+      <h2 className="text-xl md:text-2xl lg:text-3xl font-bold mb-1">
+        {Heading}
+      </h2>
+      <p className="text-muted-foreground mb-3">{description}</p>
+      <div className="w-full">{children}</div>
+      {link && linkHref && (
+        <div className="mx-auto w-fit mt-2">
+          <Link to={linkHref} className="underline underline-offset-3">
+            {link}
+          </Link>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Section;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,23 @@
+import Section from "@/components/shared/Section";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Section
+        className="bg-background"
+        Heading="Our Trendy Technology"
+        description="The most used fields in the labor market"
+        link="View All Courses"
+        linkHref="/courses"
+      >
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+          <div className="bg-blue-500">tt</div>
+          <div className="bg-green-500">tt</div>
+          <div className="bg-violet-500">tt</div>
+        </div>
+      </Section>
+    </div>
+  );
 };
 
 export default Home;
